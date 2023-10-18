c69eee066a5a18cafc83222562b2d39ffa2320b8
krelc <138886930+krelc@users.noreply.github.com>
2023-10-18 14:43:32 +1100

    Add files via upload
    Arduino code using UNO act as an Emergency stop. No matlab communication.

---
+++ twoButtonToggle/twoButtonToggle.ino
@@ -0,0 +1,31 @@
+#define eStopButton 2  
+#define resetButton 4
+#define eStopStatus 8 
+
+int eStopButtonState = 0;      // hold states in value
+int resetButtonState = 0;      
+bool eStopActive = false;     
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(eStopButton, INPUT);
+  pinMode(resetButton, INPUT);
+  pinMode(eStopStatus, OUTPUT);
+}
+void loop() {
+  eStopButtonState = digitalRead(eStopButton);
+  resetButtonState = digitalRead(resetButton);
+
+  // eStop pressed ONCE
+  if (eStopButtonState == HIGH && !eStopActive) {
+    digitalWrite(eStopStatus, HIGH); 
+    eStopActive = true;
+  }
+
+  // check for both buttons, turn off LED
+  if (eStopButtonState == HIGH && resetButtonState == HIGH) {
+    digitalWrite(eStopStatus, LOW); 
+    eStopActive = false;
+    delay(100); //debounce
+  }
+}
